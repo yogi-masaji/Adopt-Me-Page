c852d167f2cf486f11889a828638cab700bd5cce
yogi-masaji <yogimasaji02@gmail.com>
2022-10-29 13:33:56 +0700

    adding details page

--- src/Details.js
+++ src/Details.js
@@ -1,15 +1,42 @@
+import {Component} from 'react';
 import {useParams} from 'react-router-dom';
 
+class Details extends Component{
+    state = { loading: true };
 
+    async componentDidMount(){
+        const res = await fetch(`http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`);
+        const json = await res.json();
 
-const Details = () =>{
-    const {id} = useParams();
+        this.setState(Object.assign({
+            loading: false,
+        }, json.pets[0]));
+    }
 
-    return(
-        <div>
-        <h1>{id}</h1>
-        </div>
-    )
+    render() {
+        if(this.state.loading){
+            return <h2>Loading...</h2>
+        }
+
+        const {animal, breed, city, state, description, name, images} = this.state;
+
+        return (
+            <div className="details">
+                <div>
+                    <h1>{name}</h1>
+                    <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
+                    <button>Adopt {name}</button>
+                    <p>{description}</p>
+                </div>
+            </div>
+        );
+    }   
 }
 
-export default Details;
+const wrappedDetails = () => {
+    const params = useParams();
+    return <Details params={params} />
+}
+
+
+export default wrappedDetails;
